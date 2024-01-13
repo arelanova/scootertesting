493bd97b9b6baf81044e3b782ade9158c93698ec
arelanova <arelanova1@gmail.com>
2024-01-14 00:08:28 +0300

    add file with sql request

--- create_order_test.py
+++ create_order_test.py
@@ -5,15 +5,13 @@ import sender_test
 
 def create_order():
     current_body = data.order_content.copy()
-    current_body ["firstName"] = "Ololo"
+    current_body["firstName"] = "Ololo"
     track_num = sender_test.post_new_order(current_body)
     return str(track_num.json()["track"])
-def positive_assert():
+
+def test_create_order_successfully():
     track_num = create_order()
     current_params = data.params_get.copy()
     current_params["t"] = track_num
     response = sender_test.get_order(current_params)
     assert response.status_code == 200
-
-def test_order():
-    positive_assert()
