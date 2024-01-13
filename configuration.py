db82de32e18c9915bf32c12c3a3113f03cea6ccc
arelanova <arelanova1@gmail.com>
2024-01-13 23:57:17 +0300

    add create order test

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://c4274fb1-8b7b-4226-8927-e181fd560287.serverhub.praktikum-services.ru/"
+CREATE_ORDER = "api/v1/orders"
+REQUEST_ORDER = "api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,20 @@
+header_content = {
+        "Content-Type": "application/json"
+}
+order_content = {
+    "firstName": "Lera",
+    "lastName": "Popova",
+    "address": "Moscow",
+    "metroStation": 204,
+    "phone": "+7 800 555 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-03-03",
+    "comment": "Saske, come back to Kanoha",
+    "color": [
+        "BLACK"
+    ]
+}
+
+params_get = {
+    "t": ""
+}
